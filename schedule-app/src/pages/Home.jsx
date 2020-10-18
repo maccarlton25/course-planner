8a713017ca8d4cc5cc3c87e82b234d0c2d46b4c8
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2020-10-18 17:15:46 -0400

    adding content to home page

--- schedule-app/src/pages/Home.jsx
+++ schedule-app/src/pages/Home.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const Home = () => {
 
-    return(
+    return (
         <>
-        <h1>UNC Course Tracker</h1>
-        <ul> TODO:
-            <li>
-                what this app can do
-            </li>
-            <li>
-                majors/departments supported
-            </li>
-        </ul>
+        <div class='page-content'>
+            <h1 class='page-title'>UNC Course Tracker</h1>
+            <img src='../src/images/unc-logo.png' alt='unc logo'></img>
+            <h4>How you can use this app</h4>
+            <ul>
+                <li>
+                    Organize your schedules for your remaining time at Carolina
+                </li>
+                <li>
+                    Check your progress within a major or gen ed category
+                </li>
+            </ul>
+        </div>
         </>
     )
 }
