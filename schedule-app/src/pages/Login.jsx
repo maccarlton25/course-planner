1287f57193311610c761c6279fde54975e4bb1b0
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2020-11-12 17:49:25 -0500

    Revert "broke the site"
    This reverts commit aecba11d1c22fcfb55d476c9b710ca19f7f5fa4f.

--- schedule-app/src/pages/Login.jsx
+++ schedule-app/src/pages/Login.jsx
@@ -3,25 +3,6 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../styles/LoginSignup.css";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-
-// PORT
-const PORT = process.env.PORT || 4000;
-
-app.get("/", (req, res) => {
-  res.json({ message: "API Working" });
-});
-
-
-app.listen(PORT, (req, res) => {
-  console.log(`Server Started at PORT ${PORT}`);
-});
-
-// kmp example https://github.com/kmayerpatel/Fall2020COMP426UserLogin/tree/with_login
-
 const Login = () => {
     //<h1>Login to UNC Course Tracker</h1>
     const [email, setEmail] = useState("");
@@ -59,7 +40,7 @@ const Login = () => {
                     />
                 </Form.Group>
                 <Button block size="lg" type="submit" disabled={!validateForm()}>
-                    Log In
+                    Login
                 </Button>
             </Form>
         </div>
