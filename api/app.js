205947b7857843ed41980c5f84c79ee47cd46f94
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2020-11-12 16:53:02 -0500

    update app.js

--- api/app.js
+++ api/app.js
@@ -76,7 +76,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
-app.use('/items');
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
