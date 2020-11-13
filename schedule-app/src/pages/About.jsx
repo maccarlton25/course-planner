302b072b93d40cec90e692319d76941cf1745a08
Stephen Allen <StephenAllen@Stephens-MBP-2.lan>
2020-11-13 11:13:42 -0500

    About page updates

--- schedule-app/src/pages/About.jsx
+++ schedule-app/src/pages/About.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-
-    return(
-        <>
-        <div class='page-content'>
-            <h1 class='page-title'>About UNC Course Tracker</h1>
-            <img src='../src/images/unc-logo.png' alt='unc logo'></img>
-            <h4>About us</h4>
-            <ul>
-                <li>
-                    Oinfo on creators
-                </li>
-                <li>
-                    MERN full-stack approach:
-                    <ul>
-                        <li>MongoDB, Express.js, React, Node</li>
-                        </ul>
-                </li>
-                <li>How to customize experience</li>
-            </ul>
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="jumbotron" style={{ textAlign: "center" }}>
+          <h1 className="display-4">- UNC Course Tracker - </h1>
+          <p className="lead">
+            This is a tool aimed to simplify the course selection process while
+            studying at UNC Chapel Hill.
+          </p>
         </div>
-        <ul> TODO:
-            <li>
-                who made it, what is it for
-            </li>
-            <li>
-                describe the full-stack approach we took
-            </li>
-            <li>
-                how you can add to the db
-            </li>
-        </ul>
-        </>
-    )
-}
+        <div className="row">
+          <div className="col-8">
+            <h2 className="page-title">How to use the tool:</h2>
+            <p className="lead">
+              INSERT INSTRUCTIONS AFTER THE HOME/SCHEDULING PAGES ARE DEVELOPED
+            </p>
+          </div>
+          <div className="col-4" style={{ width: "25%" }}>
+            <div>
+              <h4>About the Creators</h4>
+              <ul>
+                <li>Stephen Allen</li>
+                <li>Mac Carlton</li>
+                <li>Jack Hebert</li>
+                <li>Rushil Shah</li>
+              </ul>
+            </div>
+            <div>
+              <h4>Our Approach: MERN Stack</h4>
+              <ul>
+                <li>MongoDB</li>
+                <li>Express.js</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default About;
