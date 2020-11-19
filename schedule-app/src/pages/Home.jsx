4f61745992b97b925518380d4856047a12cb8185
Stephen Allen <StephenAllen@Stephens-MBP-2.lan>
2020-11-18 20:38:45 -0500

    schedule/recs updates part 1

--- schedule-app/src/pages/Home.jsx
+++ schedule-app/src/pages/Home.jsx
@@ -93,7 +93,6 @@ const Home = () => {
           }
         });
       }
-      console.log(userData.user);
     }
     return result.length;
   }
--- schedule-app/src/pages/Schedule.jsx
+++ schedule-app/src/pages/Schedule.jsx
@@ -15,50 +15,98 @@ const Schedule = () => {
     fetchData();
   }, []);
 
-  const oldMajorReq = [
-    "401",
-    "410",
-    "411",
-    "455",
-    "550",
-    // "m231",
-    // "m232",
-    // "m233",
-    // "m347",
-    // "s435",
-    // "p1168",
-    // "sci",
-  ];
-  const newMajorReq = [
-    "210",
-    "211",
-    "301",
-    "311",
-    "455",
-    "550",
-    "m231",
-    "m232",
-    "m233",
-    "m347",
-    "s435",
-    "p1168",
-    "sci",
-  ];
+  const oldBSReq = ["401", "410", "411", "455", "550"];
+  const bsReq = ["m231", "m232", "m233", "m347", "s435", "p1168", "sci"];
+  const newBSReq = ["210", "211", "301", "311", "455", "550"];
+
+  const oldBAReq = ["401", "410", "411"];
+  const baReq = ["m231", "s435"];
+  const newBAReq = ["210", "211", "301", "311"];
 
   function majorReqLeft() {
     let userCourses = userData.user.coursesTaken;
-    // let otherUserCourses = userData.user.bsRequired;
     let result = [];
-    oldMajorReq.forEach((course) => {
-      if (!userCourses.includes(course)) {
-        result.push(course);
+    if (userData.user.major == "bs") {
+      let otherUserCourses = userData.user.bsRequired;
+
+      if (userCourses.includes("401")) {
+        oldBSReq.forEach((course) => {
+          if (!userCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+        bsReq.forEach((course) => {
+          if (!otherUserCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+      } else {
+        newBSReq.forEach((course) => {
+          if (!userCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+        bsReq.forEach((course) => {
+          if (!otherUserCourses.includes(course)) {
+            result.push(course);
+          }
+        });
       }
-    });
-    // oldMajorReq.forEach((course) => {
-    //   if (!otherUserCourses.includes(course)) {
-    //     result.push(course);
-    //   }
-    // });
+    } else {
+      let otherUserCourses = userData.user.bsRequired;
+      if (userCourses.includes("401")) {
+        oldBAReq.forEach((course) => {
+          if (!userCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+        baReq.forEach((course) => {
+          if (!otherUserCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+      } else {
+        newBAReq.forEach((course) => {
+          if (!userCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+        baReq.forEach((course) => {
+          if (!otherUserCourses.includes(course)) {
+            result.push(course);
+          }
+        });
+      }
+    }
+    return result;
+  }
+
+  function electivesLeft() {
+    let userCourses = userData.user.coursesTaken;
+    let result = [];
+    let output = 0;
+    if (userData.user.major == "bs") {
+      userCourses.forEach((course) => {
+        if (!oldBSReq.includes(course)) {
+          if (parseInt(course) > 420) {
+            result.push(course);
+          }
+        }
+      });
+      if (result.length >= 5) {
+        output = 0;
+      } else {
+        output = 5 - result.length;
+      }
+    } else {
+      userCourses.forEach((course) => {
+        if (!oldBAReq.includes(course)) {
+          if (parseInt(course) > 420) {
+            result.push(course);
+          }
+        }
+      });
+    }
     return result;
   }
 
@@ -76,21 +124,31 @@ const Schedule = () => {
     }
   }
 
-  function havePreReqs(course) {
+  function havePreReqs(input) {
     let userCourses = userData.user.coursesTaken;
-    let preReqs = course.prereq;
-    if (preReqs.length == 0) {
+    console.log(userCourses);
+    let obj = courses.filter((course) => course.code == parseInt(input));
+    let preReq = obj[0].prereq[0];
+    if (preReq.length == 0) {
       return true;
     }
-    let splitOr = preReqs.split("|");
-    if (!splitOr.includes("&")) {
-      splitOr.forEach((course) => {
-        userCourses.forEach((ele) => {
-          if (course.includes(JSON.stringify(ele.code))) {
-            return true;
-          }
-        });
+    for (let i = 0; i < preReq.length; i++) {
+      let splitOr = preReq.split("|");
+      let splitAnd = [];
+      splitOr.forEach((element) => {
+        splitAnd.push(element.split("&"));
       });
+      for (let i = 0; i < splitAnd.length; i++) {
+        for (let j = 0; j < splitAnd[i].length; j++) {
+          splitAnd[i][j] = splitAnd[i][j].substring(4);
+        }
+      }
+      for (let i = 0; i < splitAnd.length; i++) {
+        if (userCourses.includes(splitAnd[i][splitAnd[i].length - 1])) {
+          return true;
+        }
+      }
+      return false;
     }
   }
 
@@ -106,12 +164,11 @@ const Schedule = () => {
     <>
       <div className="container">
         <div className="row">
-          <div className="col-8">
+          <div className="col-6">
             <h3>Major Requirements Remaining:</h3>
             <div className="accordion" id="accordionExample">
               {userData.user && courses ? (
                 <>
-                  {getSuggestions()}
                   {majorReqLeft().map((num) => (
                     <div className="card z-depth-0 bordered">
                       <div className="card-header bg-dark" id="headingOne">
@@ -146,7 +203,46 @@ const Schedule = () => {
               )}
             </div>
           </div>
-          <div className="col-4"></div>
+          <div className="col-6">
+            <h3>Electives Remaining:</h3>
+            <div className="accordion" id="accordionExample">
+              {userData.user && courses ? (
+                <>
+                  {electivesLeft().map((num) => (
+                    <div className="card z-depth-0 bordered">
+                      <div className="card-header bg-dark" id="headingOne">
+                        <h5 className="mb-0">
+                          <button
+                            className="btn btn-link"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#collapseOne"
+                            aria-expanded="true"
+                            aria-controls="collapseOne"
+                          >
+                            {console.log(havePreReqs("431"))}
+                            {getCourseTitle(num)}
+                          </button>
+                        </h5>
+                      </div>
+                      <div
+                        id="collapseOne"
+                        className="collapse show"
+                        aria-labelledby="headingOne"
+                        data-parent="#accordionExample"
+                      >
+                        <div className="card-body">{getCourseDes(num)}</div>
+                      </div>
+                    </div>
+                  ))}
+                </>
+              ) : (
+                <>
+                  <h5>LOADING...</h5>
+                </>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
