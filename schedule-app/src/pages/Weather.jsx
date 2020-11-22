c8f5fca8fb4d38c187a3eac212d264657e0d030c
Jack <jh3bert@ad.unc.edu>
2020-11-22 16:16:09 -0500

    wont load daycards

--- schedule-app/src/pages/Weather.jsx
+++ schedule-app/src/pages/Weather.jsx
@@ -4,34 +4,28 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import DayCard from "../components/DayCard";
 
 const Weather = () => {
-    async function getWeather() {
-        let response = await fetch('https://api.api.openweathermap.org/data/2.5/weather?q=chapel+hill&appid=a2dca4995be86d6bc8760239ae5b8713');
-        let data = await response.json();
-        return data;
-    }
     
     let state = {
         fullData: [],
         dailyData: []
       }
     
-    let componentDidMount = () => {
+    let getWeather = () => {
         const weatherURL =
-        `http://api.openweathermap.org/data/2.5/forecast?zip=27514&units=imperial&APPID=a2dca4995be86d6bc8760239ae5b8713`;
+        `http://api.openweathermap.org/data/2.5/forecast?zip=27514&appid=a2dca4995be86d6bc8760239ae5b8713`;
     
         fetch(weatherURL)
         .then(res => res.json())
         .then(data => {
           const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
-          this.setState({
-            fullData: data.list,
-            dailyData: dailyData
-          }, () => console.log(this.state))
+          state.fullData = data.list;
+          state.dailyData = dailyData;
+          console.log(state);
         })
       }
-    
+      getWeather();
       let formatDayCards = () => {
-        return this.state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
+        return state.dailyData.map((reading, index) => <DayCard reading={reading} key={index} />)
       }
 
     return (
@@ -40,8 +34,9 @@ const Weather = () => {
         
         <div className="container-fluid">
             <div className="jumbotron star" style={{ textAlign: "center" }}>
-            <h1 className="display-4">Chapel Hill, NC </h1>
-            <div className="row justify-content-center">{formatDayCards()}
+            <h1 className="display-4">Chapel Hill, NC Forecast</h1>
+            <div className="row justify-content-center">
+              {formatDayCards()}
             </div>
             </div>
         </div>
