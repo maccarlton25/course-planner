e05972d03a2e1c4e02677513f9a21254acabdcd8
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2020-11-23 15:14:53 -0500

    Update About.jsx

--- schedule-app/src/pages/About.jsx
+++ schedule-app/src/pages/About.jsx
@@ -12,15 +12,26 @@ const About = () => {
           </p>
         </div>
         <div className="row">
-          <div className="col-8">
+          <div className="col-md-8">
             <h2 className="page-title">How to use the tool:</h2>
-            <p className="lead">
-              INSERT INSTRUCTIONS AFTER THE HOME/SCHEDULING PAGES ARE DEVELOPED
+            <h5>Setting up your profile:</h5>
+            <ul>
+              <li>Navigate to the sign-up button found at the top corner of the page</li>
+              <li>Create an account with your email</li>
+              <li>After creating an account, you will be redirected to the profile tab</li>
+              <li>Fill out the form with your major information and courses taken</li>
+            </ul>
+            <h5>Usage:</h5>
+            <p>
+              This app helps you manage your schedule. Based off of the courses you have taken so far, the app will help recommend classes to take and also track your remaining requirements.
+              You will also enjoy our catalog feature which allows you to search course offerings for the upcoming semester. 
             </p>
+            <h5>Account maintenance:</h5>
+            <p>At any time, a logged in user can update their courses taken by navigating to the Profile page and resubmitting the form.  You may also delete your profile on this page as well.</p>
           </div>
-          <div className="col-4" style={{ width: "25%" }}>
+          <div className="col-4-md" style={{ width: "25%" }}>
             <div>
-              <h4>About the Creators</h4>
+              <h4>Creators</h4>
               <ul>
                 <li>Stephen Allen</li>
                 <li>Mac Carlton</li>
